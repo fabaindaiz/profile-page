973ff6042f91522bc457dd0e6d65b491ae0ff6b5
fabaindaiz <fabiandiaz@ug.uchile.cl>
2023-04-05 09:06:39 -0400

    modify bio

--- src/app/core/models/project.ts
+++ src/app/core/models/project.ts
@@ -2,7 +2,7 @@ export interface Project {
     name: string;
     stack: { iconClasses: string, name: string };
     description: string;
-    imageUrl: string;
+    imageUrl?: string;
     sourceUrl: string;
     previewUrl: string;
     featured?: boolean;
